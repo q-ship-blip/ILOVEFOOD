f6973d08bb930643a3d4c7e29b9fd9894d832cde
Radin00700 <radin.kamyab@outlook.com>
2025-01-30 14:00:41 -0800

    Unity tech(back Bone)

---
+++ Assets/UnityTechnologies/Playground/Scripts/Attributes/CollectableAttribute.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollectableAttribute : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        string playerTag = otherCollider.gameObject.tag;
+
+        // Check if the object colliding is a player
+        if (playerTag == "Player" || playerTag == "Player2")
+        {
+            // Load the next level
+            SceneManager.LoadScene("Level1");
+
+            // Destroy the collectible object
+            Destroy(gameObject);
+        }
+    }
+}
