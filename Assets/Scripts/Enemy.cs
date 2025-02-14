d6110bffac4a0e7ffff9e27c84083d3b6147ebe2
Radin00700 <radin.kamyab@outlook.com>
2025-02-14 14:07:11 -0800

    Walls are walling

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,14 @@ public class Enemy : MonoBehaviour {
     [SerializeField] private float health;
 
     public void TakeDamage(float damage) {
+        Debug.Log(gameObject.name + " took damage: " + damage); // Debug Log
+
         health -= damage;
-        Debug.Log(health);
+        Debug.Log(gameObject.name + " health: " + health);
+
         if (health <= 0f) {
+            Debug.Log(gameObject.name + " died!");
             Destroy(gameObject);
-            Debug.Log("Enemy died");
         }
     }
 }
