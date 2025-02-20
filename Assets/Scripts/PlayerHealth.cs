414f277423ee81684442a9c6a9beb2fb31968bff
Radin00700 <radin.kamyab@outlook.com>
2025-02-20 14:29:57 -0800

    Update PlayerHealth.cs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public float health, maxHealth;
-    private float Start {
-        health = maxHealth;
-    }
-    
+
     public void TakeDamage(float amount) {
         health -= amount;
     }
